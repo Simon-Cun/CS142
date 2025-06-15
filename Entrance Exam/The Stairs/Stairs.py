4fb8eb9677c3c985a5f2cb65d2dc8a179447be2e
Simon <simoncun2586@gmail.com>
2025-06-15 16:58:43 -0700

    comments

--- Entrance Exam/The Stairs/Stairs.py
+++ Entrance Exam/The Stairs/Stairs.py
@@ -5,13 +5,13 @@ def main():
     heights = [] # array for the stair heights
     height_differences = [] # array for the stair height differences
 
-    for i in range(n):
+    for i in range(n): # gets all the inputs into heights array
         heights.append(int(input()))
 
-    for i in range(n - 1):
+    for i in range(n - 1): # calculates all the differences into the height_differences array
         height_differences.append(heights[i] - heights[i + 1])
 
-    for i in range(n - 2):
+    for i in range(n - 2): # checks if the differences are the same, if not then print where it breaks and exit
         if(height_differences[i] != height_differences[i + 1]):
             print(i + 3)
             break
